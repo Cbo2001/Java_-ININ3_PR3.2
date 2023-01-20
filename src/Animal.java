cf97fd7a796a3c4bb5d1bd1641bee0511417f810
Sebastian Żurawski <zurawski.sebastian@gmail.com>
2023-01-20 20:47:02 +0100

    Zadanie 1 - Pets feeding process punkty 1,2,3

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,11 @@
 public class Animal {
+        final String species;
+    private Double weight;
+
+    public Animal(String species, Double weight) {
+        this.species = species;
+        this.weight = weight;
+    }
+
+
 }
