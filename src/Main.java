a6336648b0a8dead471ec817137e97f3f9aa2922
Sebastian Żurawski <zurawski.sebastian@gmail.com>
2023-01-21 11:49:24 +0100

    Zadanie 3,4 - get your salary & give yourself a rise punkt 2

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,16 @@ public class Main {
         azor.takeFofAWalk();
 
         Car fiat = new Car("fiat", "bravo", 2010, 312321.5);
-        System.out.println("\nDodawanie AUTA\n  Model auta: " + fiat.model);
+        System.out.println("\nZadanie 2 Dodawanie AUTA\n  Model auta: " + fiat.model);
         System.out.println("  Przebieg: " + fiat.millage);
-        Human andrzej = new Human();
+        Human andrzej = new Human(2000.50);
         andrzej.setAuto(fiat);
         System.out.println("Samochód Andrzeja to  " + andrzej.getAuto());
         System.out.println(andrzej);
         System.out.println(fiat);
+
+        System.out.println("\nZadania 3 i 4");
+        andrzej.getSalary();
     }
 
 }
--- src/creatures/Human.java
+++ src/creatures/Human.java
@@ -1,14 +1,30 @@
 package creatures;
 import devices.Car;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Human {
     Animal pet;
     private Car auto;
+    private Double salary= 2000.0;
 
-    private Double salary;
+    public Human(Double salary) {
+        this.salary = salary;
+    }
+
     public Car getAuto() {
         return auto;
     }
+
+    public Double getSalary() {
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println("\nData/godz pobrania danych: " + dateTime.format(DateTimeFormatter.ofPattern("d.M.y HH:mm:ss")));
+        System.out.print("Aktualna wartość wypłaty: " + this.salary);
+        return this.salary;
+    }
+
+
 
     public void setAuto(Car auto) {
         this.auto = auto;
